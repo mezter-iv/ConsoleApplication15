82f8a2fbfcf5b322d0d1240b15833702741e7c60
Your Name <you@example.com>
2025-03-13 20:42:31 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApplication15/ConsoleApplication15.cpp
@@ -0,0 +1,13 @@
+﻿using namespace std;
+#include <iostream>
+
+void fast_print() {
+    for (int i; i < 100; i++) {
+        cout << i + 1 << ". " << "Fast print" << endl;
+    }
+}
+
+int main()
+{
+    fast_print();
+}
